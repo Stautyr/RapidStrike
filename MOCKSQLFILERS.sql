815cd9398266e6ec3ea8d9e84ab6dfd716125c8f
JakiraW <62068633+JakiraW@users.noreply.github.com>
2022-07-18 10:20:02 -0500

    Add files via upload

---
+++ MOCKSQLFILERS.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE  rapidstrike;
+USE rapidstrike;
+CREATE TABLE coordinates(
+team_coord VARCHAR(50));
+CREATE TABLE macaddr(
+team_addr CHAR(12));
+INSERT INTO macaddr
+VALUES ('2c549188c9e3');
+INSERT INTO macaddr
+VALUES ('ab76cdeb5794');
+INSERT INTO macaddr
+VALUES ('2dc1f4b0296a');
+INSERT INTO macaddr
+VALUES ('a559e2089a4d');
+INSERT INTO coordinates
+VALUES ('29.964779° N, -90.06624° E');
+INSERT INTO coordinates
+VALUES ('29.961748° N, -90.06742° E');
+INSERT INTO coordinates
+VALUES ('29.958048° N, -90.064125° E');
+INSERT INTO coordinates
+VALUES ('29.955658° N, -90.07588° E');
