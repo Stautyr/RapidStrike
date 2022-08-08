d6a1dd8cc6d0f435fce2f4e4f606992f26061886
JakiraW <62068633+JakiraW@users.noreply.github.com>
2022-08-08 10:28:10 -0500

    Add files via upload
    MainActivity for the database portion of the app.  The buttons and text box are given functionality.

--- MainActivity.java
+++ MainActivity.java
@@ -1,26 +1,61 @@
-package com.example.teamnames2;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView rapidStrike;
-    String teamList[] = {"RS1", "RS2", "RS3", "RS4", "RS5", "RS6", "RS7", "RS8", "RS9"};
+    DatabaseHelper db;
+    EditText editText;
+    Button btnAdd, btnView, btnView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rapidStrike = (ListView)findViewById(R.id.contactList);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.contacts, R.id.teamName, teamList);
-        rapidStrike.setAdapter(arrayAdapter);
+        db = new DatabaseHelper(this);
+
+        editText = (EditText)findViewById(R.id.editText);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnView = (Button)findViewById(R.id.btnView);
+        btnView2 = (Button)findViewById(R.id.btnView2);
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewDB.class);
+                startActivity(intent);
+            }
+        });
+        btnAdd.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String newEntry = editText.getText().toString();
+                        if(editText.length() != 0) {
+                            AddData(newEntry);
+                            editText.setText("");
+                        }else
+                            Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
+    public void AddData(String newEntry) {
+         {
+                    boolean isInserted = db.insertData(newEntry);
+                    if(isInserted == true)
+                        Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                }
+            }
+
+
 }
