6cec7ff92916034e950141c08126e42f59bab022
JakiraW <62068633+JakiraW@users.noreply.github.com>
2022-11-03 12:22:54 -0500

    Back button implementation

--- RapidStrikeApp/app/src/main/java/com/example/rapidstrikeapp/ViewHelp.java
+++ RapidStrikeApp/app/src/main/java/com/example/rapidstrikeapp/ViewHelp.java
@@ -1,12 +1,24 @@
 package com.example.rapidstrikeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewHelp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.help_page);
-}}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_page);
+        final Button back = findViewById(R.id.Back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(ViewHelp.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
